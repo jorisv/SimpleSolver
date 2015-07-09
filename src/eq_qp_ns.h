79d81bbf70807f0c0a3c35eb6e985320bfaed675
Joris Vaillant <joris.vaillant@gmail.com>
2015-07-09 15:00:34 +0200

    Put macro in macros.h.

--- src/eq_qp_ns.h
+++ src/eq_qp_ns.h
@@ -6,11 +6,8 @@
 #include <Eigen/Cholesky>
 #include <Eigen/QR>
 
-#if defined(EIGEN_RUNTIME_NO_MALLOC)
-  #define EQP_CHECK_MALLOC(x) Eigen::internal::set_is_malloc_allowed(!x)
-#else
-  #define EQP_CHECK_MALLOC(x)
-#endif
+// EQP
+#include "macros.h"
 
 
 namespace Eigen
