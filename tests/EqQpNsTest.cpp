9c16a1665d4e6d9ae0b389cb684b63e2bf505b14
Joris Vaillant <joris.vaillant@gmail.com>
2015-07-10 21:05:08 +0200

    Set LGPL license.

--- tests/EqQpNsTest.cpp
+++ tests/EqQpNsTest.cpp
@@ -1,10 +1,25 @@
+// This file is part of SimpleSolver.
+//
+// SimpleSolver is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// SimpleSolver is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public License
+// along with SimpleSolver.  If not, see <http://www.gnu.org/licenses/>.
+
 // includes
 // std
 #include <iostream>
 
 // boost
 #define BOOST_TEST_DYN_LINK
-#define BOOST_TEST_MODULE Algo
+#define BOOST_TEST_MODULE EqQpNs
 #include <boost/test/unit_test.hpp>
 
 // Eigen
